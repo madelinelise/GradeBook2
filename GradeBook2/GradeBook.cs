c02147417bfc8642198bb882ead4eb40c3b66703
Madeline Schimenti <madelinelise@gmail.com>
2015-09-14 08:27:55 -0400

    Created GradeBook Class
    Fig. 5.6
    Gradebook Class that solves the class average problem using counter
    controlled repetition.

---
+++ GradeBook2/GradeBook.cs
@@ -0,0 +1,30 @@
+﻿// Fig. 5.6
+// Gradebook Class that solves the class average problem using counter controlled repetition.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GradeBook2
+{
+    public class GradeBook
+    {
+        //  auto implemented property courseName
+        public string courseName { get; set; }
+
+        // constructor initializes CourseName property
+        public GradeBook (string name)
+        {
+            courseName = name;
+        } // end constructor
+
+        public void DisplayMessage()
+        {
+            // property CourseName gets the name of the course
+            Console.WriteLine("Welcome to the grade book for\n{0}!\n", courseName);
+        } // end method DisplayMessage
+
+    } // end Gradebook class
+}
